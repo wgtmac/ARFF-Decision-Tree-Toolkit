d4b2d3f1346a6bb2d5d1ec633be7ea799661c17b
dgzzhb <TianZheng@cmu.edu>
2015-03-29 21:37:24 -0400

    seems done

--- src/EntropyCalculator.java
+++ src/EntropyCalculator.java
@@ -4,68 +4,143 @@ import java.util.HashSet;
 
 public class EntropyCalculator {
 
-	private String target;
 	private DataSet dataSet;
-	private HashSet<String> valueSet;
-	
-	//temp
-	public HashSet<String> getValueSet() {
-		return valueSet;
-	}
 
-	public void setValueSet(HashSet<String> valueSet) {
-		this.valueSet = valueSet;
-	}
-
-	public EntropyCalculator(DataSet dataSet, String target) {
+	public EntropyCalculator(DataSet dataSet) {
 		this.dataSet = dataSet;
-		this.target = target;
-		valueSet = dataSet.getAttributeMap().get(target).getValueSet();
-		if (isContinuous(target)) {
-			discretize(target);
-		}
 	}
 		
-	public double getTargetEntropy() {
+	/**
+	 * Discrete stand alone Entropy, target or normal
+	 * @param attr the desired attribute
+	 * @return entropy of the attribute
+	 */
+	public double getEntropy(String attr) {
 		double sumEntropy = 0;
-
+		HashSet<String> valueSet = dataSet.getAttributeMap().get(attr).getValueSet();
 		for (String value : valueSet) {
-			sumEntropy += getValueEntropy(target, value);
+			sumEntropy += getValueEntropy(attr, value);
 		}
 				
 		return sumEntropy;
 	}
 	
 
-
-	public double getAttributeEntropy(String attr) {
-		if (isContinuous(attr)) {
-			discretize(attr);
-		}
+	/**
+	 * Discrete conditional entropy
+	 * @param attr the desired attribute
+	 * @param target the target attribute
+	 * @return attribute entropy at target
+	 */
+	public double getCondEntropy(String attr, String target) {
 		
 		double sumEntropy = 0;
 		HashSet<String> attrValueSet = dataSet.getAttributeMap()
 									   .get(attr).getValueSet();		
 		for (String attrValue : attrValueSet) {
-			sumEntropy += getAttrValueGain(attr, attrValue);
+			sumEntropy += getCondProduct(attr, attrValue, target);
 		}
 		
 		return sumEntropy;
 	}
 	
+	/**
+	 * Continuous stand alone Entropy, target or normal
+	 * @param attr the desired attribute
+	 * @param cut the cut point for continuous value
+	 * @return entropy of the attribute
+	 */
+	public double getContinuousEntropy(String attr, String cut) {
+		double totalEntries = dataSet.getData().size();
+		double leftSum = 0;
+		double rightSum = 0;
+		
+		for (Data data : dataSet.getData()) {
+			String value = data.getData().get(attr);
+			if (Double.valueOf(value) <= Double.valueOf(cut)) {
+				leftSum++;
+			} else {
+				rightSum++;
+			}
+		}	
+		double leftPossibility = leftSum / totalEntries;
+		double rightPossibility = rightSum / totalEntries;
+		return -(leftPossibility * getLog(leftPossibility)) 
+				- (rightPossibility * getLog(rightPossibility));
+	}
+	
+	/**
+	 * Continuous conditional entropy
+	 * @param attr the desired attribute
+	 * @param cut the cut point for continuous value
+	 * @return entropy of the attribute
+	 */
+	public double getContinuousCondEntropy(String attr, String cut, String target) {
+		HashSet<String> valueSet = dataSet.getAttributeMap().get(target).getValueSet();
+		double totalEntries = dataSet.getData().size();
+		
+		double leftTotalSum = 0;
+		double rightTotalSum = 0;		
+		for (Data data : dataSet.getData()) {
+			String value = data.getData().get(attr);
+			if (Double.valueOf(value) <= Double.valueOf(cut)) {
+				leftTotalSum++;
+			} else {
+				rightTotalSum++;
+			}
+		}	
+
+		double leftEntropy = 0;
+		double rightEntropy = 0;
+		for (String targetValue : valueSet) {
+			double[] entropy = getContinuousValueEntropy(attr, cut, target, targetValue);
+			leftEntropy += entropy[0];
+			rightEntropy += entropy[1];
+		}
+
+		double leftTotalPoss = leftTotalSum / totalEntries;
+		double rightTotalPoss = rightTotalSum / totalEntries;		
+		return leftTotalPoss * leftEntropy + rightTotalPoss * rightEntropy;
+	}
+	
+	// Get entropy for one value while target is found
+	private double[] getContinuousValueEntropy(String attr, 
+			String cut, String target, String targetValue) {
+		double[] entropy = new double[2];
+		int leftCount = 0;
+		int rightCount = 0;
+		for (Data data : dataSet.getData()) {
+			if (Double.valueOf(data.getData().get(attr)) <= Double.valueOf(cut)
+			    && data.getData().get(target).equals(targetValue)) leftCount++;
+			if (Double.valueOf(data.getData().get(attr)) > Double.valueOf(cut)
+				    && data.getData().get(target).equals(targetValue)) rightCount++;
+		}
+		double leftPossibility = (double) leftCount / dataSet.getData().size();
+		if (leftPossibility == 0) entropy[0] = 0;
+		else entropy[0] = -(leftPossibility * getLog(leftPossibility));
+		double rightPossibility = (double) rightCount / dataSet.getData().size();
+		if (rightPossibility == 0) entropy[1] = 0;
+		else entropy[1] = -(rightPossibility * getLog(rightPossibility));
+		
+		return entropy;
+	}
+	
+	
 	// Get possibility * entropy for one value while target is found
-	private double getAttrValueGain(String attr, String value) {
+	private double getCondProduct(String attr, String value, String target) {
 		double sumEntropy = 0;
 		double possibility = getPossibility(attr, value);
+		HashSet<String> valueSet = dataSet.getAttributeMap().get(target).getValueSet();
 		for (String targetValue : valueSet) {
-			double entropy = getAttrValueEntropy(attr, value, targetValue);
+			double entropy = getCondValueEntropy(attr, value, target, targetValue);
 			sumEntropy += entropy;
 		}
 		return possibility * sumEntropy;
 	}
 	
 	// Get entropy for one value while target is found
-	private double getAttrValueEntropy(String attr, String value, String targetValue) {
+	private double getCondValueEntropy(String attr, 
+			String value, String target, String targetValue) {
 		int valueCount = 0;
 		ArrayList<Data> dataList = dataSet.getData();
 		for (Data data : dataSet.getData()) {
@@ -98,23 +173,15 @@ public class EntropyCalculator {
 		return Math.log(x) / Math.log(2);
 	}
 	
-	// To check whether attribute is continuous
-	private boolean isContinuous(String attr) {
-		
-		return false;
-	}
-	
-	// To discretize continuous attribute
-	private void discretize(String attr) {
-		
-	}
-	
 	public static void main(String[] args) {
 		DataSet data = new DataSet();
 		data.readDataFromFile("trainProdSelection.arff");
-		EntropyCalculator calc = new EntropyCalculator(data, "label");
+		EntropyCalculator calc = new EntropyCalculator(data);
 		
-		System.out.println(calc.getAttributeEntropy("Type"));
+		System.out.println(calc.getEntropy("label"));
+		System.out.println(calc.getCondEntropy("label", "Type"));
+		System.out.println(calc.getContinuousEntropy("salary", "20.8"));
+		System.out.println(calc.getContinuousCondEntropy("salary", "20.8", "Type"));
 	}
 
 }
